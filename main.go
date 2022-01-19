074728380e6dd4f670d6bc44a47c9afadc6ba6ac
Levshakov Nikolai <levshakov_nikk@bk.ru>
2022-01-19 21:59:22 +0300

    finished sample gorilla mux server

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func faq(w http.ResponseWriter, r *http.Request) {
 }
 func notFound(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	w.WriteHeader((http.StatusNotFound))
+	w.WriteHeader(http.StatusNotFound)
 	fmt.Fprint(w, "The page does not excist! Sorry(")
 }
 
